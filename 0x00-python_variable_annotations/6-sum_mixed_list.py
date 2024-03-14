70575f085a3a3a941dd99b8b99e7dff939d6116d
Gman <matolagarnet@gmail.com>
2024-03-14 22:57:31 +0000

    syntax fix

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""  function gets sum of mixed array annd returns as float """
+""" function gets sum of mixed array annd returns as float """
 from typing import Union, List
 
 
 def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """ function to sum a mixed list 
+    """ function to sum a mixed list
     Args:
         mxd_lst (List): [list of floats/integers]
     Returns:
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Function that returns a tuple. """
-from  typing import Union, Tuple
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float):
