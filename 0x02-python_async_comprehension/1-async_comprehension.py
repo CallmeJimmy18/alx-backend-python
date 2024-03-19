22c3efe9f055a17114a4d1400edb8f9e05847fca
Gman <matolagarnet@gmail.com>
2024-03-19 14:54:41 +0000

    syntax fix

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,12 +2,12 @@
 """ Writes a coroutine called async_comprehension """
 import asyncio
 import random
-
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> float:
+async def async_comprehension() -> List[float]:
     """
         The coroutine will collect 10 random numbers using
         an async comprehensing over async_generator
